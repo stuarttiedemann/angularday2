16a88f140c516a825ef7330a48ff20c8d5ebf8b9
stuarttiedemann <stuarttiedemann@yahoo.com>
2015-11-11 11:09:00 -0500

    Made a sortable and reversible table using Angular

--- controller.js
+++ controller.js
@@ -14,31 +14,38 @@ angular.module('myApp',[]).controller('myController',function($scope){
 	$scope.students = [
 		{
 			name: 'Freddy',
-			desc: 'Made a cool football game'
+			desc: 'Made a cool football game',
+			luckyNumber:9
 		},
 		{
 			name: 'Chance',
-			desc: 'Bootstrap Guy'
+			desc: 'Bootstrap Guy',
+			luckyNumber:11
 		},
 		{
 			name: 'Chase',
-			desc: 'Alter ego for Chance'
+			desc: 'Alter ego for Chance',
+			luckyNumber:153
 		},
 		{
 			name: 'Will',
-			desc: 'The wig guy'
+			desc: 'The wig guy',
+			luckyNumber:154
 		},
 		{
 			name: 'Stephen',
-			desc: 'Awesome theme'
+			desc: 'Awesome theme',
+			luckyNumber: 40000000
 		},
 		{
 			name: 'Griffin',
-			desc: 'DOTA2 addict'
+			desc: 'DOTA2 addict',
+			luckyNumber:63
 		},
 		{
 			name: 'Oliver',
-			desc: 'Eats 8 times a day'
+			desc: 'Eats 8 times a day',
+			luckyNumber: 32
 		}
 	];
 })
